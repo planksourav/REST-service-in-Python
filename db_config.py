a0e0b15d66f06d793784091161a8338404e11821
Sourav Singha <planksourav@gmail.com>
2019-08-01 04:37:36 +0530

    adding comment

--- db_config.py
+++ db_config.py
@@ -1,3 +1,7 @@
+# We create the below db_config.py Python script under user_crud to setup the MySQL database configurations for connecting to database. 
+# We need to configure database connection with flask module and that’s why we have imported app module and setup the MySQL configuration 
+# with flask module.
+
 from app import app
 from flaskext.mysql import MySQL
 
@@ -5,7 +9,7 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'CFT^7ygvBHU*'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'roytuts'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
